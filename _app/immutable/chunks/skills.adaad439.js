c855fd5560fda8a04fa08df79c7e0a351f380669
PapayaVi <166913424+PapayaVi@users.noreply.github.com>
2024-05-02 15:52:35 +0000

    Deploying to gh-pages from @ PapayaVi/portfolio@7291f5b1913f81de3a708a7d140db1714079c48d 🚀

---
+++ _app/immutable/chunks/skills.adaad439.js
@@ -0,0 +1,41 @@
+import{A as e}from"./UIcon.1021e135.js";const r=`# Svelte
+
+---
+
+[\`Svelte\`](https://svelte.dev/) is a free and open-source front end component framework or language created by Rich Harris and maintained by the Svelte core team members. Svelte is not a monolithic JavaScript library imported by applications: instead, Svelte compiles HTML templates to specialized code that manipulates the DOM directly, which may reduce the size of transferred files and give better client performance. Application code is also processed by the compiler, inserting calls to automatically recompute data and re-render UI elements when the data they depend on is modified. This also avoids the overhead associated with runtime intermediate representations, such as virtual DOM, unlike traditional frameworks (such as React and Vue) which carry out the bulk of their work at runtime, i.e. in the browser.
+
+The compiler itself is written in TypeScript. Its source code is licensed under MIT License and hosted on GitHub.
+
+<br/>
+
+Svelte is :
+
+- compiled : Svelte shifts as much work as possible out of the browser and into your build step. No more manual optimisations — just faster, more efficient apps.
+- compact : Write breathtakingly concise components using languages you already know — HTML, CSS and JavaScript. Oh, and your application bundles will be tiny as well.
+- complete : Built-in scoped styling, state management, motion primitives, form bindings and more — don't waste time trawling npm for the bare essentials. It's all here.
+
+<br/>
+
+## Example
+
+\`\`\`ts
+<script>
+    let count = 1;
+    $: doubled = count * 2;
+<\/script>
+
+<p>{count} * 2 = {doubled}</p>
+
+<button on:click={() => count = count + 1}>Count</button>
+\`\`\`
+
+### Heading 3
+
+#### Heading 4
+
+##### Heading 5
+
+###### Heading 6
+
+> Svelte is a free and open-source front end component framework or language created by Rich Harris and maintained by the Svelte core team members.
+`;function d(i,...s){const o=Object.assign({},i);return Object.keys(o).forEach(n=>{s.includes(n)&&delete o[n]}),o}const a=i=>i,g=[a({name:"Programming Languages",slug:"pro-lang"}),a({name:"Frameworks",slug:"framework"}),a({name:"Libraries",slug:"library"}),a({name:"Languages",slug:"lang"}),a({name:"Databases",slug:"db"}),a({name:"ORMs",slug:"orm"}),a({name:"DevOps",slug:"devops"}),a({name:"Testing",slug:"test"}),a({name:"Dev Tools",slug:"devtools"}),a({name:"Markup & Style",slug:"markup-style"}),a({name:"Design",slug:"design"}),a({name:"Soft Skills",slug:"soft"})],t=i=>{const s=d(i,"category");return i.category&&(s.category=g.find(o=>o.slug===i.category)),s},c=[t({slug:"js",color:"yellow",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.",logo:e.JavaScript,name:"Javascript",category:"pro-lang"}),t({slug:"java",color:"yellow",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.",logo:e.Java,name:"Java",category:"pro-lang"}),t({slug:"css",color:"blue",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.",logo:e.CSS,name:"CSS",category:"markup-style"}),t({slug:"html",color:"orange",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent orci enim, congue sit amet justo eget, consequat sollicitudin libero. Etiam iaculis lectus tempor, hendrerit enim in, luctus arcu. Maecenas id enim et nibh ullamcorper auctor ac eu est. Donec imperdiet, diam quis malesuada faucibus, nibh ex gravida sapien, posuere pharetra nunc libero tristique turpis. Sed egestas laoreet semper. In hac habitasse platea dictumst. Praesent vitae est nec felis maximus facilisis. Duis luctus dui id urna tristique varius. Ut vulputate leo arcu, non bibendum arcu pulvinar eget. Fusce semper elit ut congue lacinia. Suspendisse magna diam, tempus vitae interdum eget, dictum vitae nisl. Praesent quis fringilla tortor. Donec vitae sagittis dui.",logo:e.HTML,name:"HTML",category:"markup-style"}),t({slug:"express",color:"green",description:r,logo:e.ExpressJs,name:"ExpressJS",category:"framework"}),t({slug:"nodejs",color:"green",description:r,logo:e.NodeJs,name:"nodeJS",category:"library"}),t({slug:"bootstrap",color:"blue",description:r,logo:e.Bootstrap,name:"Bootstrap",category:"framework"}),t({slug:"python",color:"green",description:r,logo:e.Python,name:"Python",category:"pro-lang"}),t({slug:"numpy",color:"green",description:r,logo:e.Numpy,name:"Numpy",category:"library"}),t({slug:"pandas",color:"blue",description:r,logo:e.Pandas,name:"Pandas",category:"library"}),t({slug:"mysql",color:"blue",description:r,logo:e.MySQL,name:"MySQL",category:"db"}),t({slug:"jquery",color:"blue",description:r,logo:e.Jquery,name:"jQuery",category:"library"}),t({slug:"datatables",color:"blue",description:r,logo:e.Datatables,name:"DataTables",category:"library"}),t({slug:"ejs",color:"green",description:r,logo:e.EJS,name:"EJS",category:"lang"})],b="Skills",h=(...i)=>c.filter(s=>i.includes(s.slug)),y=i=>{const s=[],o=[];return c.forEach(n=>{if(i.trim()&&!n.name.toLowerCase().includes(i.trim().toLowerCase()))return;if(!n.category){console.log(n.category),o.push(n);return}let u=s.find(m=>{var l;return m.category.slug===((l=n.category)==null?void 0:l.slug)});u||(u={items:[],category:n.category},s.push(u)),u.items.push(n)}),o.length!==0&&s.push({category:{name:"Others",slug:"others"},items:o}),s};export{y as a,h as g,c as i,b as t};
