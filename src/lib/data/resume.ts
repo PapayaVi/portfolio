0870f528610da7da2fdf4e9ef951485450928e72
reymart-vi <110107868+reymart-vi@users.noreply.github.com>
2024-07-22 12:19:04 +0800

    Update resume.ts

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,4 +1,4 @@
 
-export const data = 'https://drive.google.com/file/d/1eEd6h74ftdIw8ys2vdXO4ulZGsI2CP5N/view?usp=drive_link';
+export const data = 'https://drive.google.com/file/d/1MiuMUVp4u5C_NK8gPofQX6maX6xxUBdr/view?usp=drive_link';
 
 export const title = 'Resumé';
