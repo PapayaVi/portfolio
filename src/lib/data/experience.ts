731927d41f5e902e93036304592ef8d742be9508
reymart-vi <110107868+reymart-vi@users.noreply.github.com>
2024-09-16 11:52:31 +0800

    Update experience.ts

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -6,47 +6,50 @@ export const items: Array<Experience> = [
 	{
 		slug: 'A-software-coursework',
 		company: 'Sociov',
-		description: 'Creating awesome tools for developers.',
+		description: 'My first experience and starting point as a developer, software engineering course. Most of the time was spent on learning and staying focused trying to meet deadlines and succesfully implementing features based on the clients request.',
 		contract: ContractType.Coursework,
 		type: 'Software Development',
 		location: 'Remote',
 		period: { from: new Date(2022, 1, 1), to: new Date(2022, 4, 1) },
 		skills: getSkills('html', 'js', 'css', 'mysql','nodejs', 'datatables', 'ejs'),
-		name: 'Lead Software Developer',
+		name: 'Lead Software Developer, Team',
 		color: 'cyan',
 		links: [],
 		logo: Assets.Sociov,
-		shortDescription: 'Creating awesome tools for developers.'
+		shortDescription: 'Starting point as a developer...',
+
+
 	},
 	{
 		slug: 'B-software-coursework',
 		company: 'Microtex Simlin Enterprise',
-		description: 'Creating awesome applications for customers.',
+		description: 'This project was a great experience. A lot of repetetive tasks in data analysis and mysql query configuration, but also a lot of learning opportunities both for Web app, and PowerBI development.',
 		contract: ContractType.Coursework,
 		type: 'Software Development',
 		location: 'Hybrid',
 		period: { from: new Date(2022, 1, 31), to: new Date(2022, 12, 1) },
-		skills: getSkills('html', 'js', 'css', 'bootstrap','mysql','nodejs'),
-		name: 'Software Developer',
+		skills: getSkills('html', 'js', 'css', 'bootstrap','mysql','nodejs','python','pandas'),
+		name: 'Software Developer, Team',
 		color: 'red',
 		links: [],
 		logo: Assets.SimlinEnterprise,
-		shortDescription: 'Creating awesome applications for customers.'
+		shortDescription: 'This project was a great experience...',
+
 	},
 	{
 		slug: 'A-software-internship',
 		company: 'CyberQ Group',
-		description: 'Created an Interactive Dashboard.',
+		description: 'This project proved to be the most exciting for me. Apply everything i have learned, best practices, and my own creativity.',
 		contract: ContractType.Internship,
 		type: 'Data Visualization',
 		location: 'Remote',
 		period: { from: new Date(2020, 11, 30), to: new Date(2021, 4, 1) },
 		skills: getSkills('python', 'numpy', 'pandas'),
-		name: 'PowerBI developer',
+		name: 'PowerBI developer, Team',
 		color: 'blue',
 		links: [],
 		logo: Assets.CyberQ,
-		shortDescription: 'Created an Interactive Dashboard.'
+		shortDescription: 'Sales Interactive Dashboard. This project proved to be the most exciting for me...'
 	}
 ];
 
